738aad4ef239015025de2029b80b2b4bcc6ac360
amrabdalfatah <amr.abdalfatah.mohamed@gmail.com>
2022-09-22 03:52:40 +0200

    Add Restful Api

---
+++ data.js
@@ -0,0 +1,55 @@
+module.exports = function() {
+    return {
+        products: [
+            {
+                id: 1, 
+                name: 'Kayak', 
+                category: 'Watersports', 
+                description: 'A boat for one person',
+                price: 275,
+            },
+            {
+                id: 2, 
+                name: 'Lifejacket', 
+                category: 'Watersports', 
+                description: 'Protective and Fashionable',
+                price: 48.95,
+            },
+            {
+                id: 3, 
+                name: 'Soccer Ball', 
+                category: 'Soccer', 
+                description: 'FIFA-approved size and weight',
+                price: 19.5,
+            },
+            {
+                id: 4, 
+                name: 'Corner Flags', 
+                category: 'Soccer', 
+                description: 'Give your playing field a proffesional touch',
+                price: 34.95,
+            },
+            {
+                id: 5, 
+                name: 'Stadium', 
+                category: 'Soccer', 
+                description: 'Flat-packed 35000 seat stadium',
+                price: 79500,
+            },
+            {
+                id: 6, 
+                name: 'Thinking Cap', 
+                category: 'Chess', 
+                description: 'Improve brain efficiency by 75%',
+                price: 16,
+            },
+            {
+                id: 7, 
+                name: 'Bling King', 
+                category: 'Chess', 
+                description: 'Gold-platted',
+                price: 1200,
+            },
+        ]
+    };
+}
