76a11ae3a7b9c23913b19b00b841f2fad72d0758
amrabdalfatah <amr.abdalfatah.mohamed@gmail.com>
2022-09-24 07:39:00 +0200

    Add Auth and Admin Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { StoreModule } from './store/store.module';
-
 import { AppComponent } from './app.component';
-import { StoreComponent } from './store/store.component';
-import { CheckoutComponent } from './store/checkout/checkout.component';
-import { CartDetailComponent } from './store/cartDetail/cartDetail.component';
-import { StoreFirstGuard } from './storeFirst.guard';
-
+import { StoreModule } from "./store/store.module";
+import { StoreComponent } from "./store/store.component";
+import { CheckoutComponent } from "./store/checkout/checkout.component";
+import { CartDetailComponent } from "./store/cartDetail/cartDetail.component";
+import { RouterModule } from "@angular/router";
+import { StoreFirstGuard } from "./storeFirst.guard";
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    StoreModule,
+  declarations: [AppComponent],
+  imports: [BrowserModule, StoreModule,
     RouterModule.forRoot([
-      { path: 'store', component: StoreComponent, canActivate: [StoreFirstGuard]},
-      { path: 'cart', component: CartDetailComponent, canActivate: [StoreFirstGuard]},
-      { path: 'checkout', component: CheckoutComponent, canActivate: [StoreFirstGuard]},
-      { path: '**', redirectTo: '/store'},
-    ]),
-  ],
+      {
+        path: "store", component: StoreComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      {
+        path: "cart", component: CartDetailComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      {
+        path: "checkout", component: CheckoutComponent,
+        canActivate: [StoreFirstGuard]
+      },
+      {
+        path: "admin",
+        loadChildren: () => import("./admin/admin.module")
+          .then(m => m.AdminModule),
+        canActivate: [StoreFirstGuard]
+      },
+      { path: "**", redirectTo: "/store" }
+    ])],
   providers: [StoreFirstGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-// Step 2
-
